f1aacab213be3a8e7e0036d64463bf1ffc2658b9
Kevin Albertson <kevin.albertson@mongodb.com>
2024-03-20 10:21:16 -0400

    use returned `encrypted_fields` for automatic encryption

--- investigations/JAVA-5377/create_encrypted_collection.py
+++ investigations/JAVA-5377/create_encrypted_collection.py
@@ -13,31 +13,39 @@ def main():
 
     # Create a collection with automatically generated keys:
     client = MongoClient()
+    client["db"].drop_collection("coll")  # Drop to clean from prior test runs.
     client_encryption = ClientEncryption(
         kms_providers,
         key_vault_namespace,
         client,
         client.codec_options,
     )
-    encrypted_fields = {
-        "fields": [
-            {
-                "keyId": None,  # Specify `None` (BSON null) to have driver create key.
-                "path": "encryptedIndexed",
-                "bsonType": "string",
-                "queries": {"queryType": "equality"},
-            }
-        ],
-    }
-    client["db"].drop_collection("coll")  # Drop to clean from prior test runs.
-    client_encryption.create_encrypted_collection(
-        client["db"], "coll", encrypted_fields=encrypted_fields, kms_provider="local"
+    _, encrypted_fields = client_encryption.create_encrypted_collection(
+        client["db"],
+        "coll",
+        encrypted_fields={
+            "fields": [
+                {
+                    "keyId": None,  # As input, specify `None` (BSON null) to have driver create key.
+                    "path": "encryptedIndexed",
+                    "bsonType": "string",
+                    "queries": {"queryType": "equality"},
+                }
+            ],
+        },
+        kms_provider="local",
     )
+    # The returned `encrypted_fields` contains the `keyId` filled in.
+    assert encrypted_fields["fields"][0]["keyId"] is not None
     client_encryption.close()
     client.close()
 
-    # Create a new client with automatic encryption. Do not specify `encryptedFieldMap` to fetch `encryptedFields` from the server.
-    opts = AutoEncryptionOpts({"local": {"key": local_master_key}}, key_vault_namespace)
+    # Create a new client with automatic encryption with the created `encrypted_fields`.
+    opts = AutoEncryptionOpts(
+        kms_providers,
+        key_vault_namespace,
+        encrypted_fields_map={"db.coll": encrypted_fields},
+    )
     encrypted_client = MongoClient(auto_encryption_opts=opts)
     encrypted_coll = encrypted_client["db"]["coll"]
     # Insert with automatic encryption.
