c5003620af26fcbcf27623e886c176d5d58b4693
Kevin Albertson <kevin.albertson@mongodb.com>
2025-03-03 08:28:19 -0500

    add both forms

---
+++ investigations/keyAltName.py
@@ -0,0 +1,63 @@
+import os
+from pymongo import MongoClient
+from pymongo.encryption import Algorithm, ClientEncryption
+from pymongo.encryption_options import AutoEncryptionOpts
+
+def main():
+    local_master_key = os.urandom(96)
+    kms_providers = {"local": {"key": local_master_key}}
+
+    # The MongoClient used to access the key vault (key_vault_namespace).
+    key_vault_client = MongoClient()
+    key_vault = key_vault_client["db"]["keyvault"]
+    key_vault.drop()
+
+    client_encryption = ClientEncryption(
+        kms_providers=kms_providers,
+        key_vault_namespace="db.keyvault",
+        key_vault_client=key_vault_client,
+        codec_options=key_vault_client.codec_options
+    )
+
+    test_key_id = client_encryption.create_data_key(
+        "local", key_alt_names=["test_key_name"]
+    )
+
+    schema = {
+        "properties": {
+            "with_key_id": {
+                "encrypt": {
+                    "keyId": [test_key_id], # An array of one UUID
+                    "bsonType": "string",
+                    "algorithm": Algorithm.AEAD_AES_256_CBC_HMAC_SHA_512_Deterministic,
+                }
+            },
+            "with_key_alt_name": {
+                "encrypt": {
+                    "keyId": "/key_name", # JSON Pointer referring to "key_name" field. The "key_name" field value is expected to refer to to a key_alt_name.
+                    "bsonType": "string",
+                    "algorithm": Algorithm.AEAD_AES_256_CBC_HMAC_SHA_512_Random, # Use Random. Deterministic with JSON Pointer is unsupported by server: "cannot have a keyId represented by a JSON pointer"
+                }
+            }
+        },
+        "bsonType": "object",
+    }
+    
+    schema_map = {"db.coll": schema}
+
+    auto_encryption_opts = AutoEncryptionOpts(
+        kms_providers, "db.keyvault", schema_map=schema_map
+    )
+
+    client = MongoClient(auto_encryption_opts=auto_encryption_opts)
+    coll = client["db"]["coll"]
+    coll.drop()
+
+    coll.insert_one({"with_key_id": "foo"})
+    coll.insert_one({"with_key_alt_name": "bar", "key_name": "test_key_name"})
+    for doc in coll.find():
+        print (doc)
+
+
+if __name__ == "__main__":
+    main()
