f6762b38b76083e64207eebc6d8ae44ddcb05edd
Kevin Albertson <kevin.albertson@mongodb.com>
2025-12-29 09:01:29 -0500

    revise example

--- investigations/create-keyAltName.py
+++ investigations/create-keyAltName.py
@@ -6,23 +6,20 @@ from pymongo.encryption_options import AutoEncryptionOpts
 kms_providers = {"local": {"key": os.urandom(96)}}
 key_vault_namespace = "keyvault.datakeys"
 
-local_master_key = os.urandom(96)
-kms_providers = {"local": {"key": local_master_key}}
-key_vault_namespace = "keyvault.datakeys"
-
-auto_encryption_opts = AutoEncryptionOpts(
-    kms_providers,
-    key_vault_namespace,
-)
 uri = os.environ.get("MONGODB_URI", "mongodb://localhost:27017")
 
 # Drop pre-existing data:
 client = MongoClient(uri)
 client.db.coll.drop()
 
-# Try to create a QE collection with an unencrypted client:
+# Try to create a QE collection with an encrypted client:
+auto_encryption_opts = AutoEncryptionOpts(
+    kms_providers,
+    key_vault_namespace,
+    # encrypted_fields_map is not set.
+)
 encrypted_client = MongoClient(uri, auto_encryption_opts=auto_encryption_opts)
-client.db.create_collection("coll", encrypted_fields={"fields": [
+encrypted_client.db.create_collection("coll", encrypted_fields={"fields": [
     {
         "path": "secret",
         "bsonType": "string",
